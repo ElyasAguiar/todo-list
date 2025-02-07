48e6a684e3056429cf239cea54c1bfd0b6315cfd
ElyasAguiar <elyas.santana98@academico.ifs.edu.br>
2025-02-06 21:20:13 -0300

    feat: add category field to task admin and form for improved task management

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 
+from .models import Task, Category
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["title", "describe", "created", "datecompleted", "important", "user", "category"]
+    readonly_fields = ["created"]
+
+
+admin.site.register(Task)
+admin.site.register(Category)
